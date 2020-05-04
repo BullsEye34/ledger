1bc35284aa3455469fd5793f848f36492f2a139a
Vamshi Prasad <vamvijkum@gmail.com>
2020-05-04 16:15:35 +0530

    add back image

---
+++ android/app/src/main/kotlin/ga/vamshiprasad/ledger/MainActivity.kt
@@ -0,0 +1,6 @@
+package ga.vamshiprasad.ledger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: app(),
+    ),
+  ));
+}
+
+class app extends StatefulWidget {
+  @override
+  _appState createState() => _appState();
+}
+
+class _appState extends State<app> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.dark,
+        statusBarColor: Colors.transparent,
+      ),
+    );
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/placeholder.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
